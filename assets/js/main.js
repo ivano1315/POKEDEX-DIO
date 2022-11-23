2066f68966693ca86fee37a7d2e2873c9936c487
ivano1315 <ivano_1315@outlook.com>
2022-11-23 09:05:37 -0300

    alterado lista dinamica

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,9 @@
 
-
 const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-function convertPokemonToHtml(pokemon) {
+function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
             <span class="number">#001</span>
@@ -24,15 +23,17 @@ function convertPokemonToHtml(pokemon) {
 
 }
 
+const pokemonList = document.getElementById('pokemonList')
+
+
 fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .then((pokemonList) => {
+    .then((pokemons) => {
 
-       for (let i = 0; i < pokemonList.length; i++) {
-        const pokemon = pokemonList[i];
-        console.log(convertPokemonToHtml(pokemon))
-        
+       for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        pokemonList.innerHTML += convertPokemonToLi(pokemon)
        }
 
     })
